a18de4339164230bf91db69ab32b5e612a98ee94
paulBlartCallMop <188939308+paulBlartCallMop@users.noreply.github.com>
2024-11-25 16:49:27 -0500

    Add match winning message

--- javascript.js
+++ javascript.js
@@ -16,10 +16,10 @@ function playRound(humanChoice,computerChoice) {
         console.log("Tie!");
     } else if ((humanChoice === "Rock" && computerChoice === "Scissors")||(humanChoice === "Scissors" && computerChoice === "Paper")||(humanChoice === "Paper" && computerChoice === "Rock")) {
         humanScore += 1;
-        document.querySelector('#result').textContent = `You win! ${humanChoice} beats ${computerChoice}.`;
+        document.querySelector('#result').textContent = (humanScore === 5) ? "The human has won the match!" : `You win! ${humanChoice} beats ${computerChoice}.`
     } else {
         computerScore += 1;
-        document.querySelector('#result').textContent = `You lose! ${computerChoice} beats ${humanChoice}.`
+        document.querySelector('#result').textContent = (computerScore === 5) ? "The computer has won the match!": `You lose! ${computerChoice} beats ${humanChoice}.`
     }
     document.querySelector('#manScore').textContent = humanScore;
     document.querySelector('#botScore').textContent = computerScore;
